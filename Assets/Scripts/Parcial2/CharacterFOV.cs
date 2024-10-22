0a1c6a7d81a3046808e490a75ed24282fcd4f5e8
santiagoPostacchini <postacchini.santiago.et21.21@gmail.com>
2024-10-22 20:30:34 -0300

    Fov Gizmo

--- Assets/Scripts/Parcial2/CharacterFOV.cs
+++ Assets/Scripts/Parcial2/CharacterFOV.cs
@@ -30,4 +30,24 @@ public class CharacterFOV : MonoBehaviour
 
         return false;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireSphere(transform.position, _radius);
+
+        Gizmos.color = Color.red;
+
+        Vector3 LineA = GetVectorFromAngle(_angle * 0.5f + transform.eulerAngles.y);
+        Vector3 LineB = GetVectorFromAngle(-_angle * 0.5f + transform.eulerAngles.y);
+
+        Gizmos.DrawLine(transform.position, transform.position + LineA);
+        Gizmos.DrawLine(transform.position, transform.position + LineB);
+    }
+
+    Vector3 GetVectorFromAngle(float angle)
+    {
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
 }
